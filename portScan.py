321ff8264900c7c5d2dd9d710dfb16225defd04f
mylonly <tianxianggen@gmail.com>
2016-06-20 21:39:11 +0800

    commit change

--- portScan.py
+++ portScan.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
 # 端口扫描
+# 2016-6-20:增加多线程支持
+# 2016-6-20:增加nmap包
 
 import optparse
 import sys
 from socket import *
-reload(sys)
-sys.setdefaultencoding('utf8')
+from threading import *
+import nmap
 
+screenLock = Semaphore(value=1)
 def connScan(targetHost,targetPort):
     try:
         connSkt = socket(AF_INET,SOCK_STREAM)
         connSkt.connect((targetHost,targetPort))
         connSkt.send('ViolentPython\r\n')
         results = connSkt.recv(100)
+        screenLock.acquire()
         print '[+]%d/tcp open' % targetPort
-        print '[+] '+str(results).encode('utf-8').decode('utf-8')
-        connSkt.close()
+        print '[+] '+str(results)
     except:
+        screenLock.acquire()
         print '[-]%d/tcp closed' % targetPort
-        
+    finally:
+        screenLock.release()
+        connSkt.close()
+
+
 def portScan(targetHost,targetPorts):
     try:
         targetIp = gethostbyname(targetHost)
@@ -35,8 +43,14 @@ def portScan(targetHost,targetPorts):
     setdefaulttimeout(1)
 
     for targetPort in targetPorts:
-        print 'Scanning port '+ targetPort
-        connScan(targetHost,int(targetPort)) 
+        t = Thread(target=connScan,args=(targetHost,int(targetPort)))
+        t.start()
+
+def nmapScan(targetHost,targetPort):
+    nmScan = nmap.PortScanner()
+    nmapScan.scan(targetHost,targetPort)
+    state = nmScan[targetHost]['tcp'][int(targetPort)]['state']
+    print("[*]"+targetHost+" tcp/"+targetPort+" "+state)
 
 def main():
     parse = optparse.OptionParser('Usage %prog -H'+'<target host> -p <targe port>')
@@ -50,7 +64,8 @@ def main():
     if (targetHost == None) | (targetHost == None):
         print '[-] You Must specify a target host and port[s].'
         exit(0)
-    
-    portScan(targetHost,targetPorts)
+    for tgtPort in targetPorts:
+        nmapScan(targetHost,tgtPort)
+    # portScan(targetHost,targetPorts)
 if __name__ == '__main__':
     main()
